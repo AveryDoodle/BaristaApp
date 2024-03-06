78ad0e7a921e89daa48b0282c2800a9622d68871
Avery McAllister <avery@bradandsylver.com>
2024-03-06 09:06:04 -0600

    Initial Commit

---
+++ BaristaAppUITests/BaristaAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BaristaAppUITestsLaunchTests.swift
+//  BaristaAppUITests
+//
+//  Created by Avery McAllister on 3/6/24.
+//
+
+import XCTest
+
+final class BaristaAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
