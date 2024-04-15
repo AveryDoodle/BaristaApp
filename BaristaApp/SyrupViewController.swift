9a8767de1fa27e72fb3f616364a997646930103d
Avery McAllister <avery@bradandsylver.com>
2024-04-15 10:12:15 -0500

    Original

---
+++ BaristaApp/SyrupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SyrupViewController.swift
+//  BaristaApp
+//
+//  Created by Avery McAllister on 3/11/24.
+//
+
+import UIKit
+
+class SyrupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
