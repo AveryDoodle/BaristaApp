47e37d09005d3b4fca89775adc3d867ed77bfd0b
Avery McAllister <avery@bradandsylver.com>
2024-04-18 09:26:38 -0500

    added vc stuff

---
+++ BaristaApp/CheckViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CheckViewController.swift
+//  BaristaApp
+//
+//  Created by Avery McAllister on 4/18/24.
+//
+
+import UIKit
+
+class CheckViewController: UIViewController {
+    
+    
+    @IBOutlet weak var correctOutlet: UILabel!
+    
+    @IBOutlet weak var yoursOutlet: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+}
