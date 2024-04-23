431d0a7732b2303a0d3fc2d8be2860b48fb39ecc
Avery McAllister <avery@bradandsylver.com>
2024-04-23 09:47:19 -0500

    modified storyboard

---
+++ BaristaApp/HelpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HelpViewController.swift
+//  BaristaApp
+//
+//  Created by Avery McAllister on 4/23/24.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+    
+    
+    @IBOutlet weak var infoOutlet: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
