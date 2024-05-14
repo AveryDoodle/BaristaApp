3738ec167b03b2c3ba6eb0e63b100608a081029d
Avery McAllister <avery@bradandsylver.com>
2024-05-14 09:17:55 -0500

    added alert

--- BaristaApp/ViewController.swift
+++ BaristaApp/ViewController.swift
@@ -13,9 +13,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
 
         
     }
+    
+    @IBAction func quizAction(_ sender: Any) {
+        if AppData.drinks.isEmpty{
+     
+            let alert = UIAlertController(title: "Error!", message: "must add drinks before going to quiz", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Continue", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+            present(alert, animated: true, completion: nil)
+        } else {
+       
+            
+            performSegue(withIdentifier: "tooQuiz" , sender: nil)
+           
+        }
+    }
+    
 
 
 }
